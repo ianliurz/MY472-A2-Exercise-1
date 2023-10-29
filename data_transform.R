412b907cfb0c933849688298edbdc938f8c3ffd9
ianliurz <ianliurz@gmail.com>
2023-10-29 23:21:58 +0000

    Update the function by adding a parameter so that the function can specify a particular number of chicken for each time frame

--- data_transform.R
+++ data_transform.R
@@ -3,11 +3,16 @@ library(tidyverse)
 # A dataset that records the weight of chicken under different diets
 ChickWeight
 
-# Create a function to rank the weight of chicken for all dates
-rank_chicken_by_date <- function(data) {
+# Create a function to rank the n heaviest chicken of all dates
+rank_chicken_by_date <- function(data, number_of_heaviest) {
   ranked_chickenweight <- data |> 
-    arrange(Time, desc(weight)) # rearrange the rows by time and weight in descending order
+    # rearrange the rows by time and weight in descending order
+    arrange(Time, desc(weight)) |> 
+    group_by(Time) |> 
+    # take the first n rows of each group, n specified by the parameter
+    slice_head(n = number_of_heaviest)
 }
 
-chick_weight <- rank_chicken_by_date(ChickWeight)
+# test the function
+chick_weight <- rank_chicken_by_date(ChickWeight, 5)
 chick_weight
